c068ecf4798c5e0a7fb9699830d4a62aa07044a3
Don Yu <37088038+devzons@users.noreply.github.com>
2023-06-03 14:01:52 -0700

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className='text-5xl font-semibold'>Homepage</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam nihil sed laborum earum ad dolores nisi ipsam! Modi culpa cum distinctio illum, molestias id quis dolorum ex maxime impedit consectetur.</p>
+    </div>
+  )
+}
+
+export default Home
